49e1e1fcd228182f04fb500a52e4728b822ef9bd
Gnxlo <gonzalo.nsqp@gmail.com>
2025-08-25 13:06:26 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -11,6 +11,7 @@ const JUMP_AND_RUN_GAME: Game = {
   playUrl: '/jump-and-run.html',
   instructions: 'Tap the screen or press the Spacebar to make the character jump. Your goal is to navigate through the gaps in the obstacles. Each pair of obstacles you successfully pass will increase your score. The game ends if you hit an obstacle, the top, or the bottom of the screen.',
   aspectRatio: '9 / 16',
+  nativeWidth: 360,
 };
 
 const COLOR_JUMP_GAME: Game = {
@@ -24,6 +25,7 @@ const COLOR_JUMP_GAME: Game = {
   playUrl: '/color-jump.html',
   instructions: 'Tap the screen to make the ball jump. You can only pass through the part of the obstacle that matches your ball\'s color. Hitting a different color will end the game. Pass through color changers to switch your color and keep going. The higher you go, the higher your score!',
   aspectRatio: '9 / 16',
+  nativeWidth: 400,
 };
 
 const NUMBER_MERGE_GAME: Game = {
@@ -37,6 +39,7 @@ const NUMBER_MERGE_GAME: Game = {
   playUrl: '/number-merge.html',
   instructions: 'Use your arrow keys or swipe (on touch devices) to move all tiles in a direction. When two tiles with the same number touch, they merge into one, doubling their value. A new tile (either a 2 or a 4) appears on the board after every move. The game ends when the board is full and no more moves are possible. Your goal is to achieve the highest score and create the highest numbered tile.',
   aspectRatio: '1 / 1',
+  nativeWidth: 500,
 };
 
 export const FEATURED_GAME: Game = JUMP_AND_RUN_GAME;
