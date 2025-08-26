881b0e20bc8e94b1ad05ee734737afd061defefb
Gnxlo <gonzalo.nsqp@gmail.com>
2025-08-26 15:14:17 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,24 +1,32 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import GamePage from './pages/GamePage';
+import ExplorePage from './pages/ExplorePage';
+import PopularPage from './pages/PopularPage';
+import GenresPage from './pages/GenresPage';
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-[#0a0a0a] text-gray-200 font-sans flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/game/:gameId" element={<GamePage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-        </Routes>
-      </main>
-      <Footer />
+    <div className="min-h-screen bg-[#0a0a0a] text-gray-200 font-sans">
+      <Sidebar />
+      <div className="lg:ml-64 flex flex-col min-h-screen">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/explore" element={<ExplorePage />} />
+            <Route path="/popular" element={<PopularPage />} />
+            <Route path="/genres" element={<GenresPage />} />
+            <Route path="/game/:gameId" element={<GamePage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
